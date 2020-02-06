6e31d49e2202c3017a33cf63ba6dab8b4015eac5
张成蹊 <zhangchengxi@bytedance.com>
2020-02-06 19:08:27 +0800

    check hold_config file when initializing

--- initializer.py
+++ initializer.py
@@ -1,6 +1,7 @@
 # 初始化文件：根据传入的用户token来进行API调用
 #   用户token在TuShare中被用于标识用户的权限，不同权限的账户能获得的信息量不同
 #   请确保账户拥有未来操作所需要的权限
+import os
 import argparse
 
 import tushare as ts
@@ -15,6 +16,7 @@ args = parser.parse_args()
 
 
 def main():
+    # 设置个人token
     if args.token is None:
         raise AttributeError("请在调用该初始化文件时传入用户token。")
 
@@ -23,6 +25,10 @@ def main():
     print("  * 如果需要重置token，重新运行当前文件即可。")
     ts.set_token(args.token)
     print("设置用户token成功！")
+
+    # 检查是否存在持仓数据
+    if not os.path.exists("hold_config.json"):
+        print("----- 警告：没有找到持仓配置文件，请确保根目录下存在名为'hold_config.json'的配置文件。 -----")
 
 
 if __name__ == '__main__':
